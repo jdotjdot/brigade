36d6a42fa7d5a05e57ea2040ea0404f05b6f3857
JJ <JJ@jdotjdot.com>
2018-03-26 13:13:04 -0400

    feat(vcs-sidecar): Add Github App authentication to the Github VCS sidecar

--- git-sidecar/rootfs/askpass.sh
+++ git-sidecar/rootfs/askpass.sh
@@ -1,2 +1,18 @@
 #!/bin/sh
-echo $BRIGADE_REPO_AUTH_TOKEN
+
+# If a BRIGADE_REPO_AUTH_TOKEN is provided, return that
+if [ ! -z "${BRIGADE_REPO_AUTH_TOKEN+x}" ] ; then
+  echo $BRIGADE_REPO_AUTH_TOKEN
+
+# Else, if all of the proper environmental variables
+#  to use a Github App are provided, use that instead
+#  to authenticate against Github and get a limited-lifetime
+#  OAuth token
+elif [ ! -z "${BRIGADE_REPO_GITHUB_APP_IDENTIFIER+x}" ] && \
+	[ ! -z "${BRIGADE_REPO_GITHUB_APP_INSTALLATION_ID+x}" ] && \
+	[ ! -z "${BRIGADE_REPO_GITHUB_APP_PRIVATE_KEY+x}" ] ; then
+  curl -s -XPOST "https://api.github.com/installations/$BRIGADE_REPO_GITHUB_APP_INSTALLATION_ID/access_tokens" \
+	  -H 'Accept: application/vnd.github.machine-man-preview+json' \
+	  -H "Authorization: Bearer $(bash /jwt-auth.sh RS256 '{"iss": "'$BRIGADE_REPO_GITHUB_APP_IDENTIFIER'"}' "$BRIGADE_REPO_GITHUB_APP_PRIVATE_KEY")" \
+		| jq -r .token
+fi
